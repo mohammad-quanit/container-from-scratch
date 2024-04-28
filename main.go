cf9c9540b465c7f49437a1e42826d64a8f70b4d2
Mohammad-Quanit <muhammadquanit@gmail.com>
2024-04-29 00:35:04 +0500

    add: mount process

--- main.go
+++ main.go
@@ -19,14 +19,12 @@ func main() {
 }
 
 func parent() {
-	fmt.Printf("Runing %v as %d\n", os.Args[2:], os.Getpid())
-
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// Creating a new sperate process with shared UNIX time
+	// Creating a new sperate process with shared UNIX time and separate PID
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
@@ -37,22 +35,28 @@ func parent() {
 }
 
 func child() {
-	fmt.Printf("Runing %v as %d\n", os.Args[2:], os.Getpid())
+	fmt.Printf("Runing %v as PID %d\n", os.Args[2:], os.Getpid())
 
-	syscall.Sethostname([]byte("my-container"))
-	syscall.Chroot("/")
-	syscall.Chdir("/")
-	syscall.Mount("proc", "proc", "proc", 0, "")
+	must(syscall.Sethostname([]byte("til-container")))
+	must(syscall.Chroot("/"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	// must(syscall.Mount("FS", "FS", "tmpfs", 0, ""))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
+	must(cmd.Run())
 
-	syscall.Unmount("proc", 0)
+	must(syscall.Unmount("proc", 0))
+	// must(syscall.Unmount("FS", 0))
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 }
